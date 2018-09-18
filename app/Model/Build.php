f6cc29b612b561c69f29d0678830bdc675123bc5
Bryon Bean <bryon.bean@kitware.com>
2018-09-18 10:28:27 -0400

    refactor: changes notification previously sent handling

--- app/Model/Build.php
+++ app/Model/Build.php
@@ -2986,9 +2986,13 @@ class Build
         return $this->BuildEmailCollection->get($category);
     }
 
-    public function SetBuildEmailCollection(BuildEmailCollection $collection)
+    public function SetBuildEmailCollection(BuildEmailCollection $collection, $category)
     {
-        $this->BuildEmailCollection = $collection;
+        if (!$this->BuildEmailCollection) {
+            $this->BuildEmailCollection = new CollectionCollection();
+        }
+
+        $this->BuildEmailCollection->addItem($collection, $category);
     }
 
     public function SetUpdate(BuildUpdate $update)
