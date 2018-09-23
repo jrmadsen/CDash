1e4a21d70c653eb9113edb2aa83007e027fda30d
Bryon Bean <bryon.bean@kitware.com>
2018-09-23 15:18:14 -0400

    fix: fixes subproject test and trilinos test

--- tests/test_subproject.php
+++ tests/test_subproject.php
@@ -14,6 +14,8 @@
   PURPOSE. See the above copyright notices for more information.
 =========================================================================*/
 
+use CDash\Config;
+
 class SubProjectTestCase extends KWWebTestCase
 {
     public function __construct()
@@ -53,10 +55,38 @@ class SubProjectTestCase extends KWWebTestCase
         if (!$this->submission('SubProjectExample', $file)) {
             return;
         }
-        if (!$this->compareLog($this->logfilename, $rep . '/cdash_1.log')) {
-            return;
+        $url = Config::getInstance()->getBaseUrl();
+        $expected = [
+            'simpletest@localhost',
+            'FAILED (w=21): SubProjectExample/NOX - Linux-GCC-4.1.2-SERIAL_RELEASE - Nightly' ,
+            'A submission to CDash for the project SubProjectExample has warnings.',
+            "Details on the submission can be found at {$url}/viewProject?projectid=",
+            'Project: SubProjectExample',
+            'SubProject: NOX',
+            'Site: godel.sandia.gov',
+            'Build Name: Linux-GCC-4.1.2-SERIAL_RELEASE',
+            'Build Time: 2009-08-06 12:19:56',
+            'Type: Nightly',
+            'Total Warnings: 21',
+            '*Warnings* (first 5 included)',
+            "packages/epetraext/src/transform/EpetraExt_BlockAdjacencyGraph.cpp ({$url}/viewBuildError.php?type=",
+            '/home/rabartl/PROJECTS/dashboards/Trilinos.base/SERIAL_RELEASE/Trilinos/packages/epetraext',
+            "packages/epetraext/src/block/EpetraExt_BlockDiagMatrix.cpp ({$url}/viewBuildError.php?type=",
+            'EpetraExt_BlockDiagMatrix.cpp: In member function ‘virtual void EpetraExt_BlockDiagMatrix::Print(std::ostream&) const',
+            '/home/rabartl/PROJECTS/dashboards/',
+            "packages/epetraext/src/block/EpetraExt_MultiPointModelEvaluator.cpp ({$url}/viewBuildError.php?type=",
+            'EpetraExt_MultiPointModelEvaluator.h:',
+            "packages/galeri/src/Galeri_Utils.cpp ({$url}/viewBuildError.php?type=",
+            'In function ‘void Galeri::Solve',
+            '/home/rabartl/PROJECTS/dashboard',
+            "packages/galeri/src/Galeri_CrsMatrices.cpp ({$url}/viewBuildError.php?type=",
+            'In function ‘Epetra_CrsMatrix* Galeri::Matrices::UniFlow2D',
+            '/',
+            '-CDash on cdash.dev',
+        ];
+        if ($this->assertLogContains($expected, 32)) {
+            $this->pass('Passed');
         }
-        $this->pass('Test passed');
     }
 
     public function testSubmissionSubProjectTest()
@@ -67,9 +97,39 @@ class SubProjectTestCase extends KWWebTestCase
         if (!$this->submission('SubProjectExample', $file)) {
             return;
         }
-        if (!$this->compareLog($this->logfilename, $rep . '/cdash_2.log')) {
-            return;
+        $url = Config::getInstance()->getBaseUrl();
+        $expected = [
+            'nox-noemail@noemail',
+            'FAILED (t=1): SubProjectExample/NOX - Linux-GCC-4.1.2-SERIAL_RELEASE - Nightly',
+            'A submission to CDash for the project SubProjectExample has failing tests',
+            "Details on the submission can be found at {$url}/viewProject?projectid=",
+            'Project: SubProjectExample',
+            'SubProject: NOX',
+            'Site: godel.sandia.gov',
+            'Build Name: Linux-GCC-4.1.2-SERIAL_RELEASE',
+            'Build Time: 2009-08-06 12:19:56',
+            'Type: Nightly',
+            'Total Failing Tests: 1',
+            '*Failing Tests*',
+            "NOX_FiniteDifferenceIsorropiaColoring | Completed (Failed) | ({$url}/testDetails.php?test=",
+            '-CDash on cdash.dev',
+            'simpletest@localhost',
+            'FAILED (t=1): SubProjectExample/NOX - Linux-GCC-4.1.2-SERIAL_RELEASE - Nightly',
+            'A submission to CDash for the project SubProjectExample has failing tests',
+            'Details on the submission can be found at http://cdash.dev/viewProject?projectid=',
+            'Project: SubProjectExample',
+            'SubProject: NOX',
+            'Site: godel.sandia.gov',
+            'Build Name: Linux-GCC-4.1.2-SERIAL_RELEASE',
+            'Build Time: 2009-08-06 12:19:56',
+            'Type: Nightly',
+            'Total Failing Tests: 1',
+            '*Failing Tests*',
+            "NOX_FiniteDifferenceIsorropiaColoring | Completed (Failed) | ({$url}/testDetails.php?test=",
+            '-CDash on cdash.dev',
+        ];
+        if ($this->assertLogContains($expected, 39)) {
+            $this->pass('Passed');
         }
-        $this->pass('Test passed');
     }
 }
